dfe7926ab3861c2dcd77f5db68f391f20f48050a
Vladimir Zotov <zott14@outlook.com>
2018-03-26 16:58:04 +0300

    Program is created,

--- countBetweenNum/Program.cs
+++ countBetweenNum/Program.cs
@@ -10,6 +10,27 @@ namespace countBetweenNum
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
+
+            for (double i = a; a < b; a++)
+            {
+                if ((a % 2) != 0)
+                {
+                    Console.WriteLine("{0} ", a);
+                }
+                result += a;
+            }
+
+            Console.WriteLine("Сумма чисел равна: {0}", result);
+
+            // Delay
+
+            Console.ReadKey();
         }
     }
 }
